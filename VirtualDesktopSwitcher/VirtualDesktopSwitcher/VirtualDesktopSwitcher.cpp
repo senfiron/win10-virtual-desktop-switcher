1a0d265684a70696af09d5d31e5e2694e880b6d1
Vladislav Novikov <senfiron@gmail.com>
2015-09-06 15:20:00 +0300

    Implemented [Win+Tab] and [Win+Shift+Tab] hotkeys to switch virtual desktops

--- VirtualDesktopSwitcher/VirtualDesktopSwitcher/VirtualDesktopSwitcher.cpp
+++ VirtualDesktopSwitcher/VirtualDesktopSwitcher/VirtualDesktopSwitcher.cpp
@@ -1,8 +1,115 @@
-#include "VirtualDesktops.h"
+﻿#include "VirtualDesktops.h"
+
+IVirtualDesktopManagerInternal* pDesktopManager = nullptr;
+HHOOK hKeyboardHook = nullptr;
+bool reverse = false;
+
+LRESULT CALLBACK KeyHandler(int nCode, WPARAM wParam, LPARAM lParam);
+UINT SimulateInput(WORD wKeyCode, DWORD dwFlags = 0);
+HRESULT GetVirtualDesktopManager(IVirtualDesktopManagerInternal** ppDesktopManagerInternal);
+HRESULT SwitchVirtualDesktop(IVirtualDesktopManagerInternal *pDesktopManager, bool reverse = false);
+
+int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+{
+    HRESULT hr = GetVirtualDesktopManager(&pDesktopManager);
+
+    if (FAILED(hr))
+        return 1;
+
+    hKeyboardHook = SetWindowsHookEx(WH_KEYBOARD_LL, KeyHandler, hInstance, 0);
+
+    MSG msg;
+
+    // Main message loop:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    UnhookWindowsHookEx(hKeyboardHook);
+    pDesktopManager->Release();
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK KeyHandler(int nCode, WPARAM wParam, LPARAM lParam) {
+
+    if (GetAsyncKeyState(VK_LWIN) != 0)
+    if (nCode >= 0 && (wParam == WM_KEYDOWN || wParam == WM_KEYUP))
+    {
+        KBDLLHOOKSTRUCT kbdStruct = *((KBDLLHOOKSTRUCT*)lParam);
+        HWND hTaskView = FindWindow("MultitaskingViewFrame", NULL);
+
+        switch (wParam)
+        {
+        case WM_KEYDOWN:
+            if (kbdStruct.vkCode == VK_TAB)
+            {
+                //TODO: Find another way to open Task View 
+
+                if (GetAsyncKeyState(VK_SHIFT) != 0 && reverse == false)
+                {
+                    /*
+                    Achtung! Костыль!
+                    Task View doesn't open when you press [Win + Shift + Tab]
+                    Release [Shift] to fix this, but remeber reverse direction
+                    */
+
+                    reverse = true;
+                    if (hTaskView == NULL)
+                        SimulateInput(VK_SHIFT, KEYEVENTF_KEYUP);
+                }
+
+                SwitchVirtualDesktop(pDesktopManager, reverse);
+
+                //Interrupt [Tab] to prevent Task View closing
+                if (hTaskView != NULL)
+                    return 1;
+            }
+            break;
+
+        case WM_KEYUP:
+            if (hTaskView != NULL)
+            {
+                if (kbdStruct.vkCode == VK_LWIN)
+                {
+                    //TODO: Find another way to close Task View 
+                    SimulateInput(VK_ESCAPE);
+                    reverse = false;
+                }
+                else if (kbdStruct.vkCode == VK_LSHIFT)
+                {
+                    reverse = false;
+                }
+            }
+            break;
+
+        default:
+            break;
+        }
+    }
+
+    return CallNextHookEx(hKeyboardHook, nCode, wParam, lParam);
+}
+
+UINT SimulateInput(WORD wKeyCode, DWORD dwFlags)
+{
+    INPUT ip;
+
+    ip.type = INPUT_KEYBOARD;
+    ip.ki.wScan = 0;
+    ip.ki.time = 0;
+    ip.ki.dwExtraInfo = 0;
+
+    ip.ki.wVk = wKeyCode;
+    ip.ki.dwFlags = dwFlags; // 0 for key press
+
+    return SendInput(1, &ip, sizeof(INPUT));
+}
 
 HRESULT GetVirtualDesktopManager(IVirtualDesktopManagerInternal** ppDesktopManagerInternal)
 {
-    ::CoInitialize(NULL);
+    ::CoInitializeEx(NULL, COINIT_MULTITHREADED);
 
     IServiceProvider* pServiceProvider = nullptr;
     HRESULT hr = ::CoCreateInstance(
@@ -17,7 +124,7 @@ HRESULT GetVirtualDesktopManager(IVirtualDesktopManagerInternal** ppDesktopManag
     return hr;
 }
 
-HRESULT SwitchVirtualDesktop(IVirtualDesktopManagerInternal *pDesktopManager, bool reverse = false)
+HRESULT SwitchVirtualDesktop(IVirtualDesktopManagerInternal *pDesktopManager, bool reverse)
 {
     IVirtualDesktop *pCurrentDesktop = nullptr;
     HRESULT hr = pDesktopManager->GetCurrentDesktop(&pCurrentDesktop);
@@ -28,7 +135,7 @@ HRESULT SwitchVirtualDesktop(IVirtualDesktopManagerInternal *pDesktopManager, bo
 
         AdjacentDesktop direction = reverse ? AdjacentDesktop::LeftDirection : AdjacentDesktop::RightDirection;
         hr = pDesktopManager->GetAdjacentDesktop(pCurrentDesktop, direction, &pNextDesktop);
-        
+
         if (FAILED(hr))
         {
             IObjectArray *pObjectArray = nullptr;
@@ -61,21 +168,4 @@ HRESULT SwitchVirtualDesktop(IVirtualDesktopManagerInternal *pDesktopManager, bo
     }
 
     return hr;
-}
-
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
-{
-    IVirtualDesktopManagerInternal* pDesktopManager = nullptr;
-    HRESULT hr = GetVirtualDesktopManager(&pDesktopManager);
-
-    if (SUCCEEDED(hr))
-    {
-        SwitchVirtualDesktop(pDesktopManager);
-        ::Sleep(1000);
-        SwitchVirtualDesktop(pDesktopManager, true);
-
-        pDesktopManager->Release();
-    }
-
-    return 0;
 }
